522035ca1a71c62e4f5a3454d0949f6811ad8eb0
KartikMahendra <vim1991@gmail.com>
2020-03-12 19:41:29 -0500

    add fade in effect for gallery elements when scrolling

--- js/home-page.js
+++ js/home-page.js
@@ -66,3 +66,21 @@ function GetHeight() {
     document.documentElement["offsetHeight"]
   );
 }
+
+
+// Fade in elements on scroll
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop()
+  var pageBottom = pageTop + $(window).height()
+  var tags = $("section")
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible")
+    } else {
+      $(tag).removeClass("visible")
+    }
+  }
+})
